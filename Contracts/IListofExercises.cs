92a7abb7cb5b3a6ffeca6fc66b98a9614bef9a30
Lucas José <lucasjosesilva012@gmail.com>
2025-06-14 13:02:20 -0300

    Adicionado pasta de Interfaces

---
+++ Contracts/IListofExercises.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENTRA21_Listas_de_Exercícios_Lógica_de_Programação.Contracts
+{
+    public interface IListofExercises
+    {
+        string Title();
+
+        void Execute();
+    }
+}
